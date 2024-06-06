23f3925d61d20f21a53e559c7dfa9e9fe65f3ee8
josefinaaSolorzano <167581166+josefinaaSolorzano@users.noreply.github.com>
2024-06-05 22:00:09 -0300

    cambio keras + clasif
    05/06

--- SoleMate.py
+++ SoleMate.py
@@ -4,37 +4,52 @@ import numpy as np
 import streamlit as st
 from openai import OpenAI
 import pandas as pd
+import os
 
 def classify_fruit(img):
+
+    # Disable scientific notation for clarity
     np.set_printoptions(suppress=True)
 
-    model = load_model("keras_model.h5", compile=False)
+    # Load the model
+    model_path = os.path.join("keras_model.h5")
+    model = load_model(model_path, compile=False)
 
-    with open("labels.txt", "r") as f:
-        class_names = f.readlines()
-        class_names = [c.strip() for c in class_names]  # Remove any surrounding whitespace
+    # Load the labels
+    labels_path = os.path.join("labels.txt")
+    class_names = open(labels_path, "r").readlines()
 
+    # Create the array of the right shape to feed into the keras model
+    # The 'length' or number of images you can put into the array is
+    # determined by the first position in the shape tuple, in this case 1
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
+    # Replace this with the path to your image
     image = img.convert("RGB")
 
+    # resizing the image to be at least 224x224 and then cropping from the center
     size = (224, 224)
     image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
 
+    # turn the image into a numpy array
     image_array = np.asarray(image)
 
+    # Normalize the image
     normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
 
+    # Load the image into the array
     data[0] = normalized_image_array
 
+    # Predicts the model
     prediction = model.predict(data)
     index = np.argmax(prediction)
     class_name = class_names[index]
     confidence_score = prediction[0][index]
 
+
     return class_name, confidence_score
 
-def generate_recipe(label):
+#def generate_recipe(label):
     client = OpenAI(api_key="")
 
     response = client.Completion.create(
