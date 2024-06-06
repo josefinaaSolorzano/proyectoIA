03b8193f83fbae0f328f2355bb8f134216511349
josefinaaSolorzano <167581166+josefinaaSolorzano@users.noreply.github.com>
2024-06-06 20:06:03 -0300

    recomendaciones texto

--- SoleMate.py
+++ SoleMate.py
@@ -45,10 +45,12 @@ def classify_fruit(img):
 
 # Dictionary of recommendations for each class
 recommendations = {
-    "0 Air Forces": "Compra esta zapatilla - Air Forces",
-    "1 Air Max": "Compra esta zapatilla - Air Max",
-    "2 Dunk": "Compra esta zapatilla - Dunk",
-    # Add more classes and recommendations as needed
+    "0 Air Forces": "Comodidad: Excelente amortiguación gracias a la unidad Air en el talón, proporcionando comodidad para uso diario.Talles: Disponibles en una amplia gama de talles para hombres, mujeres y niños. Colores: Variedad de colores y ediciones limitadas, desde el clásico blanco y negro hasta combinaciones de colores vibrantes y colaboraciones con artistas. Uso: Perfectas para el uso diario, moda urbana y casual. Perfil del Usuario: Ideal para jóvenes y adultos que buscan un estilo casual y moderno, amantes de la moda urbana y coleccionistas de zapatillas.",
+    "1 Air Jordans": "Comodidad: Diseñadas para ofrecer soporte y comodidad durante el juego de baloncesto, con tecnología de amortiguación avanzada. Talles: Disponibles en talles para hombres, mujeres y niños. Colores: Diversidad de colores y ediciones limitadas, con combinaciones que suelen contar con la participación del propio Michael Jordan. Uso: Originalmente para baloncesto, ahora también usadas como zapatillas de moda. Perfil del Usuario: Apreciadas por jugadores de baloncesto, coleccionistas y aficionados a la moda deportiva.",
+    "2 Air Maxes": "Comodidad: Excelente amortiguación y soporte gracias a la unidad Air Max, ideales para largas caminatas y uso diario. Talles: Disponibles en talles para toda la familia. Colores: Amplia gama de colores y estilos, desde tonos neutros hasta combinaciones audaces. Uso: Perfectas para el uso diario, running y actividades físicas ligeras. Perfil del Usuario: Adecuadas para personas activas que valoran la comodidad y el estilo deportivo, tanto jóvenes como adultos.",
+    "3 Cleats": "Comodidad: Sujeción firme y diseño anatómico para un ajuste perfecto, con tecnologías que proporcionan estabilidad y comodidad durante el juego. Talles: Disponibles en talles para hombres, mujeres y niños. Colores: Variedad de colores adaptados a los equipos y gustos personales. Uso: Específicas para deportes de campo como fútbol, béisbol y fútbol americano.Perfil del Usuario: Deportistas que practican deportes de campo y buscan rendimiento, durabilidad y soporte en su calzado.",
+    "4 Dunks": "Comodidad: Suela acolchada y estructura resistente, diseñada inicialmente para el baloncesto y adaptada para el skateboard. Talles: Disponibles en una amplia variedad de talles para hombres y mujeres. Colores: Gama extensa de colores y ediciones especiales, con colaboraciones frecuentes que añaden valor coleccionable. Uso: Popular en el skateboard, moda urbana y uso casual. Perfil del Usuario: Skaters, entusiastas de la moda urbana y coleccionistas de zapatillas exclusivas.",
+    
 }
 
 st.set_page_config(layout='wide')
