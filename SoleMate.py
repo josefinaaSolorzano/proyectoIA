e4938acdbc3fcd5f61eaf8139c5b20efec4f80d2
josefinaaSolorzano <167581166+josefinaaSolorzano@users.noreply.github.com>
2024-06-05 22:05:19 -0300

    cambio clasif SoleMate.py
    05/06

--- SoleMate.py
+++ SoleMate.py
@@ -8,37 +8,45 @@ import os
 
 def classify_fruit(img):
 
+
     # Disable scientific notation for clarity
     np.set_printoptions(suppress=True)
 
+
     # Load the model
-    model_path = os.path.join("keras_model.h5")
-    model = load_model(model_path, compile=False)
+    model = load_model("keras_model.h5", compile=False)
+
 
     # Load the labels
-    labels_path = os.path.join("labels.txt")
-    class_names = open(labels_path, "r").readlines()
+    class_names = open("labels.txt", "r").readlines()
+
 
     # Create the array of the right shape to feed into the keras model
     # The 'length' or number of images you can put into the array is
     # determined by the first position in the shape tuple, in this case 1
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
+
     # Replace this with the path to your image
     image = img.convert("RGB")
+
 
     # resizing the image to be at least 224x224 and then cropping from the center
     size = (224, 224)
     image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
 
+
     # turn the image into a numpy array
     image_array = np.asarray(image)
+
 
     # Normalize the image
     normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
 
+
     # Load the image into the array
     data[0] = normalized_image_array
+
 
     # Predicts the model
     prediction = model.predict(data)
@@ -47,7 +55,15 @@ def classify_fruit(img):
     confidence_score = prediction[0][index]
 
 
+    # Print prediction and confidence score
+    # print("Class:", class_name[2:], end="")
+    # print("Confidence Score:", confidence_score)
+
+
     return class_name, confidence_score
+
+
+
 
 #def generate_recipe(label):
     client = OpenAI(api_key="")
