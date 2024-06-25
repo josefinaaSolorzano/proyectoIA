3333cfba9c2926e609a552530b2abbff5a46f099
josefinaaSolorzano <167581166+josefinaaSolorzano@users.noreply.github.com>
2024-06-25 16:55:13 -0300

    Update SoleMate.py
    fix boton url 2

--- SoleMate.py
+++ SoleMate.py
@@ -137,7 +137,7 @@ if input_img is not None:
                 # Mostrar recomendación basada en la clase
                 recommendation = recommendations.get(label.strip(), "No hay recomendación disponible para esta clase.")
                 st.write(recommendation)
-                st.markdown('[Ir a Our SoleMates](pages/Our_SoleMates.py)')
+                st.page_link('proyectoIA\pages\Our_SoleMates.py', label = "Our SoleMates")
 
 
 
